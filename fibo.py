c22f3035b300e4457b3ff28cdfac8f2dc371112e
Tereza47 <xdraho14@vutbr.cz>
2021-05-15 21:54:11 +0200

    Fibonnaci

--- fibo.py
+++ fibo.py
@@ -1,7 +1,24 @@
+def max_profit(stock_prices):
+    nb_vals = len(stock_prices)
+    if nb_vals <= 1:
+        return 0
+
+    left_profit = max_profit(stock_prices[:nb_vals // 2])
+    right_profit = max_profit(stock_prices[nb_vals // 2:])
+    left_min = min(stock_prices[:nb_vals // 2])
+    right_max = max(stock_prices[nb_vals // 2:])
+    return max(left_profit, right_profit, right_max - left_min)
+
+
+def recursive_nth_fibo(number):
+    if number <= 1:
+        return 1
+    else:
+        return recursive_nth_fibo(number-1) + recursive_nth_fibo(number-2)
+
 
 def main():
-    pass
-
+    print(recursive_nth_fibo(5))
 
 if __name__ == '__main__':
     main()
